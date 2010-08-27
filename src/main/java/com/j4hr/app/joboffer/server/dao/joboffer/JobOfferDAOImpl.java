2405ba028c8884ff46ea285ecda13547eb75b8eb
Fabrice Sznajderman <Fabszn@gmail.com>
2010-08-28 00:31:25 +0200

    ajout annotation repository

--- src/main/java/com/j4hr/app/joboffer/server/dao/joboffer/JobOfferDAOImpl.java
+++ src/main/java/com/j4hr/app/joboffer/server/dao/joboffer/JobOfferDAOImpl.java
@@ -1,8 +1,11 @@
 package com.j4hr.app.joboffer.server.dao.joboffer;
 
+import org.springframework.stereotype.Repository;
+
 import com.j4hr.app.joboffer.server.dao.AbstractJpaDAO;
 import com.j4hr.app.joboffer.shared.entities.JobOffer;
 
+@Repository
 public class JobOfferDAOImpl extends AbstractJpaDAO<Integer,JobOffer> implements JobOfferDAO {
 
 }
