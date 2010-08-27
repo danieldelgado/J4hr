eafb288777a9d24769ee3b0e0e029ecf45a1c400
Fabrice Sznajderman <Fabszn@gmail.com>
2010-08-27 23:28:21 +0200

    rajout de fichier supprimé par erreur

---
+++ src/main/java/com/j4hr/app/joboffer/server/dao/joboffer/JobOfferDAO.java
@@ -0,0 +1,8 @@
+package com.j4hr.app.joboffer.server.dao.joboffer;
+
+import com.j4hr.app.joboffer.server.dao.IDao;
+import com.j4hr.app.joboffer.shared.entities.JobOffer;
+
+public interface JobOfferDAO extends IDao<Integer, JobOffer> {
+
+}
---
+++ src/main/java/com/j4hr/app/joboffer/server/dao/joboffer/JobOfferDAOImpl.java
@@ -0,0 +1,8 @@
+package com.j4hr.app.joboffer.server.dao.joboffer;
+
+import com.j4hr.app.joboffer.server.dao.AbstractJpaDAO;
+import com.j4hr.app.joboffer.shared.entities.JobOffer;
+
+public class JobOfferDAOImpl extends AbstractJpaDAO<Integer,JobOffer> implements JobOfferDAO {
+
+}
